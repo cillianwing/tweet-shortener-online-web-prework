bf310e7b65dbbf564a00889e1f767b4ee28c77c8
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-29 01:11:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,54 +1,51 @@
 # Write your code here.
 def dictionary
-  substitute_words = {
-    :hello => "hi",
-    :to => "2",
-    :two => "2",
-    :too => "2",
-    :for => "4",
-    :four => "4",
-    :be => "b",
-    :you => "u",
-    :at => "@",
-    :and => "&"
+  {"hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
   }
 end
 
-def word_substituter(tweet_string)
-  tweet_array = tweet_string.split
-  dictionary_hash = dictionary
-  dictionary_hash.each do |k, v|
-    tweet_array.each do |word|
-      if word.downcase == k.to_s
-        word.replace(v.to_s)
-      end
+def word_substituter(string)
+  string_array = string.split
+  replace_words = dictionary
+  new_string = []
+  string_array.each do |word|
+    if replace_words.key?(word.downcase)
+      new_string << replace_words.values_at(word.downcase) 
+    else 
+      new_string << word
     end
   end
-  tweet_array.join(sep=" ")
+  new_string.join(" ")
 end
 
-def bulk_tweet_shortener(array_of_tweets)
-  array_of_tweets.each do |array|
-    puts word_substituter(array)
+def bulk_tweet_shortener(tweet_array)
+  tweet_array.each do |string|
+    puts word_substituter(string)
   end
 end
 
 def selective_tweet_shortener(tweet)
   if tweet.size > 140
-    #puts word_substituter(tweet)
-    return word_substituter(tweet)
-  else
-    #puts tweet
-    return tweet
+    word_substituter(tweet)
+  else 
+    tweet 
   end
 end
 
 def shortened_tweet_truncator(tweet)
-  new_tweet = selective_tweet_shortener(tweet)
-  if new_tweet.size > 140
-    truncated_string = "#{new_tweet[0..136]}..."
-    return truncated_string
-  else 
-    return tweet
+  short_tweet = word_substituter(tweet)
+  if short_tweet.size > 140
+    return "#{short_tweet[0..136]}..."
+  else
+    return short_tweet
   end
 end
